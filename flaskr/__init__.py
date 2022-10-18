af9ea0fad9b1fdd301c4ac85ccce1e2fa9e77bdd
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2022-10-18 09:14:06 -0500

    New Update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,8 +29,16 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import db
+    # register the database commands
+    from flaskr import db
     db.init_app(app)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
